fa533c5be1de9304addb64348e367d8954c253f6
Iragemini <iragemini@gmail.com>
2021-07-21 21:53:11 +0300

    refactor: removed nesting if

--- solutions/task2.js
+++ solutions/task2.js
@@ -2,17 +2,19 @@ function closure() {
   let num = 0;
   let k = 2;
   return function () {
-    if (num !== 0) {
-      if (num % 5 === 0) {
-        k = 3;
-        num /= 5;
-        return num;
-      }
-      if (num % 7 === 0) {
-        k = 1;
-        num -= 7;
-        return num;
-      }
+    if (num === 0) {
+      num += k;
+      return num;
+    }
+    if (num % 5 === 0) {
+      k = 3;
+      num /= 5;
+      return num;
+    }
+    if (num % 7 === 0) {
+      k = 1;
+      num -= 7;
+      return num;
     }
     num += k;
     return num;
