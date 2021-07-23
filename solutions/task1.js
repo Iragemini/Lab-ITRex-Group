c804ea1f7713ecf4200b737b7d66fb2ae2f5be71
Iragemini <iragemini@gmail.com>
2021-07-23 19:26:27 +0300

    feat: add solution to task 1

---
+++ solutions/task1.js
@@ -0,0 +1,23 @@
+console.time('lightBulbs()');
+function lightBulbs(n, p) {
+  const bulbs = new Array(n);
+  bulbs.fill(false);
+  const filtered = p.filter((item) => item <= n);
+  const inversion = (index) => {
+    bulbs[index] = !bulbs[index];
+  };
+  const len = filtered.length;
+  for (let i = 0; i < len; i += 1) {
+    bulbs.forEach((item, index) => {
+      const num = index + 1;
+      if (num % filtered[i] === 0) {
+        inversion(index);
+      }
+    });
+  }
+  const amount = bulbs.reduce((acc, item) => (item ? acc + 1 : acc), 0);
+  return amount;
+}
+console.timeEnd('lightBulbs()');
+console.log('bulbs', lightBulbs(20, [2, 3, 8, 33]));
+console.log('bulbs', lightBulbs(172, [19, 2, 7, 13, 40, 23, 16, 1, 45, 9]));
